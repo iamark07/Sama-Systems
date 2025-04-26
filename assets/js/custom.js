021a94c51c605570e412e25d7b8706afef9b9e8f
Arbaz Karimi <arbazkarimi@gmail.com>
2025-04-26 17:06:17 +0530

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,4 @@
-
-// header menu slider features 
+// header menu slider features
 
 const menuBtn = document.getElementById("menu-btn");
 const mobileMenu = document.getElementById("mobile-menu");
@@ -18,3 +17,21 @@ function toggleMenu() {
 
 menuBtn.addEventListener("click", toggleMenu);
 menuOverlay.addEventListener("click", toggleMenu);
+
+// header size small when screen scroll
+const header = document.getElementById("header-content");
+const logo = document.getElementById("logo-img");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 150) {
+    header.classList.remove("py-7");
+    header.classList.add("py-4");
+    logo.classList.remove("w-36");
+    logo.classList.add("w-28");
+  } else {
+    header.classList.remove("py-4");
+    header.classList.add("py-7");
+    logo.classList.remove("w-28");
+    logo.classList.add("w-36");
+  }
+});
