308686700c4b01624287625193a6f5d475c881bb
Arbaz Karimi <arbazkarimi@gmail.com>
2025-05-02 17:04:31 +0530

    update header

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,14 @@
+// add shadow on scroll feature 
+window.addEventListener('scroll', function () {
+  const header = document.getElementById('main-header');
+  if (window.scrollY > 10) {
+      header.classList.add('shadow-md');
+  } else {
+      header.classList.remove('shadow-md');
+  }
+});
+
+// menu slider functions click on menu btn 
 const menuToggle = document.getElementById("menu-toggle");
 const mobileMenu = document.getElementById("mobile-menu");
 const menuClose = document.getElementById("menu-close");
